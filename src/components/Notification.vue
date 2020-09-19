e1db37dda73defcd7ae0eda9ceef3f93b4c20caf
Vyacheslav Bazhenov <vyacheslav.bazhenov@nutnet.ru>
2020-09-19 19:25:49 +0400

    Фикса лага при открытии полного уведомления

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,24 +1,29 @@
 <template>
-  <v-expand-transition>
-    <v-card v-show="this.show" outlined>
-      <v-alert prominent :type="this.type" class="ma-2" v-show="!!this.actionText">
-        <v-row align="center">
-          <v-col class="grow">{{ this.text }}</v-col>
-          <v-col class="shrink">
-            <v-btn ref="actionBtn" outlined>{{ this.actionText }}</v-btn>
-          </v-col>
-        </v-row>
-      </v-alert>
-      <v-alert
-          v-if="!this.actionText"
-          dismissible
-          close-icon="mdi-close-circle"
-          :type="this.type"
-          class="ma-2">
-        {{ this.text }}
-      </v-alert>
-    </v-card>
-  </v-expand-transition>
+  <div>
+    <v-expand-transition>
+      <v-card v-show="this.showFull" outlined>
+        <v-alert prominent :type="this.type" class="ma-2" >
+          <v-row align="center">
+            <v-col class="grow">{{ this.text }}</v-col>
+            <v-col class="shrink">
+              <v-btn ref="actionBtn" outlined>{{ this.actionText }}</v-btn>
+            </v-col>
+          </v-row>
+        </v-alert>
+      </v-card>
+    </v-expand-transition>
+    <v-expand-transition>
+      <v-card v-if="this.showSimple" outlined>
+        <v-alert
+            dismissible
+            close-icon="mdi-close-circle"
+            :type="this.type"
+            class="ma-2">
+          {{ this.text }}
+        </v-alert>
+      </v-card>
+    </v-expand-transition>
+  </div>
 </template>
 
 <script>
@@ -27,23 +32,26 @@ export default {
   data: () => ({
     text: '',
     type: 'info',
-    show: false,
+    showFull: false,
+    showSimple: false,
     actionText: null
   }),
   mounted() {
     let self = this
     this.$root.$on('notify', (text, type, timeout, actionText = null, callback = null) =>  {
-      self.show = true
+      self.showSimple = !actionText
       self.text = text
       self.type = type
       self.actionText = actionText
 
       if (actionText) {
+        self.showFull = true
         self.$refs.actionBtn.click = callback
       }
 
       setTimeout(() => {
-        self.show = false
+        self.showSimple = false
+        self.showFull = false
       }, timeout)
     })
   },
